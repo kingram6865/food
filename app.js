c60162ee548266214ebbf19c67791385cb3a3fc5
Ken Ingram <ken.ingram@gmail.com>
2021-02-16 18:50:30 -0800

    Major developments to the API

--- app.js
+++ app.js
@@ -1,55 +1,115 @@
 require('dotenv').config()
-const mysql = require('mysql2')
 const pug = require('pug')
 const express = require('express')
+// const mysql = require('mysql2')
+const db = require('./model/dbcalls')
+const utils = require('./utils')
 
 const app = express()
-const DATABASE = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  database: process.env.DB
-}
 
 app.set('view engine', 'pug')
 
-async function getFoodData(ndb_no) {
-  const conn = mysql.createConnection(DATABASE)
-  let dbData
-  let SQL = "select c.*, a.tagname, b.nutr_val, a.units, a.nutr_desc, a.systematic_name, a.isomer_name, a.fatty_acid_type FROM nutrient_definition a, nutrient_data b, foods c where c.ndb_no = b.ndb_no and a.nutr_no = b.nutr_no and c.ndb_no=?"
-  const params = [ndb_no]
-  SQL = mysql.format(SQL, params)
-  // console.log(SQL)
-
-  try {
-    dbData = await conn.promise().query(SQL)
-    // console.log(`(Line 27):Data Array Length: ${dbData[0].length}`)
-  } catch (e) {
-    console.log(e)
-  } finally {
-    conn.end()
-  }
-  return dbData[0]
-}
-
 app.get('/', async (req, res) => {
-  let results = await getFoodData('44005')
+  let results = await db.getFoodData('44005')
   console.log(results)
 
-  res.render('index', {data: results})
+  // res.render('index', {data: results})
+  res.json(results)
 })
 
-app.get('/foods', (req, res) => {
-
+app.get('/food', async (req, res) => {
+  let results = await db.getFoodData('44005')
+  res.json(results[0])
 })
 
+app.get('/food/group', async (req, res) => {
+  let results = await db.listAllFoodGroups()
+  // console.log(` /food/group endpoint:${results}`)
+  res.json(results)
+})
+
+app.get('/food/:ndbno', async (req, res) => {
+  let output
+  let results = await db.getFoodData(req.params.ndbno)
+
+  results.forEach((x, i) => {
+    let nutrient_info = utils.isFat({
+      [x.nutr_desc]: {
+        tagname: x.tagname,
+        value: x.nutr_val,
+        units: x.units,
+        fatty_acid_type: x.fatty_acid_type,
+        systematic_name: x.systematic_name,
+        isomer_name: x.isomer_name
+      }
+    })
+
+    console.log(`Line 45: ${JSON.stringify(nutrient_info)}`)
+
+    if (i === 0) {
+      // console.log(i, x.long_desc)
+      output = {
+        objid: x.objid,
+        ndb_no:x.ndb_no,
+        fdgrp_cd:x.fdgrp_cd,
+        long_desc:x.long_desc,
+        short_desc:x.short_desc,
+        common_name:x.common_name,
+        mfg_name:x.mfg_name,
+        survey:x.survey,
+        ref_desc:x.ref_desc,
+        refuse:x.refuse,
+        sciName:x.sciName,
+        n_factor:x.n_factor,
+        pro_factor:x.pro_factor,
+        fat_factor:x.fat_factor,
+        cho_factor:x.cho_factor,
+        nutritional_data: []
+      }
+
+      output.nutritional_data.push(nutrient_info)
+      // output.nutritional_data.push({
+      //   [x.nutr_desc]: {
+      //     tagname: x.tagname,
+      //     value: x.nutr_val,
+      //     units: x.units,
+      //     fatty_acid_type: x.fatty_acid_type,
+      //     systematic_name: x.systematic_name,
+      //     isomer_name: x.isomer_name
+      //   }
+      // })
+
+    } else {
+      output.nutritional_data.push(nutrient_info)
+      // // console.log(i, x.nutr_desc)
+      // output.nutritional_data.push({
+      //   [x.nutr_desc]: {
+      //     tagname: x.tagname,
+      //     value: x.nutr_val,
+      //     units: x.units,
+      //     fatty_acid_type: x.fatty_acid_type,
+      //     systematic_name: x.systematic_name,
+      //     isomer_name: x.isomer_name
+      //     }
+      // })
+    }
+  })
+
+  // res.send(`Getting data for food with NDB# ${req.params.ndbno}`)
+  res.json(output)
+})
+
+
+app.get('/food/group/all/:groupid', async (req, res) => {
+  let results = await db.listFoodsByGroup(req.params.groupid)
+
+  res.json(results)
+})
+
+app.get('/food/group/:code', async(req, res) => {
+  let results = await db.listFoodGroup(req.params.code)
+  console.log(results)
+  res.json(results[0])
+})
 
 app.listen(8005, '192.168.1.19')
-
-// async function test() {
-//   let testdata = await getFoodData('44005')
-//   console.log(testdata[0])
-// }
-
-// test()
---
+++ utils/index.js
@@ -0,0 +1,33 @@
+function isFat(data) {
+  // let output = {}
+  const fats = ['4:0', '6:0', '8:0','10:0','12:0','14:0','16:0','16:1','18:0','18:1','18:2','18:3','18:4','20:1','20:4','20:5','22:1','22:5']
+
+  let index = Object.keys(data)[0]
+  // let fats = index.fatty_acid_type && systematic_name && index.isomer_name
+  let present = fats.map(x => {
+    let regex = new RegExp(x)
+    return (index.search(regex) > 0) ? true : false
+  })
+
+  console.log(`Line 14: ${JSON.stringify(present)}`)
+
+  if (present.includes(true)) {
+    output = data
+  } else {
+    // console.log(`isFat Line 10: ${JSON.stringify(data)}`)
+    output = {
+      [index]: {
+        tagname: data[index].tagname,
+        value: data[index].value,
+        units: data[index].units
+      }
+    }
+  }
+
+  return output
+}
+
+
+module.exports = {
+  isFat
+}
